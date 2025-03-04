0035301eb991ac88ceb0a3c97797804a4c739ca6
Arthur Bourgoin <arthur.bourgoin@gmail.com>
2025-03-04 18:56:27 +0100

    Setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureModels();
+    }
+
+    /**
+     * Configure the Eloquent models.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::preventLazyLoading(! $this->app->isProduction());
+        Model::preventSilentlyDiscardingAttributes(! $this->app->isProduction());
     }
 }
